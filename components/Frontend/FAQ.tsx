f15840f1b59bfb2a9f7477df266426c0becbfaa6
binarydata123 <binarydata.sale@gmail.com>
2023-03-31 14:28:01 +0530

    commit

--- components/Frontend/FAQ.tsx
+++ components/Frontend/FAQ.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import FrequentlyQuestions from './FrequentlyQuestions'
 
 export default function FAQ() {
     return (
@@ -19,7 +18,73 @@ export default function FAQ() {
                     </div>
                 </div>
             </div>
-            <FrequentlyQuestions/>
+            <section className="faq-section ptb-100">
+                <div className="container">
+                    <div className="section-title">
+                        <h2>Frequently <span>Asked</span> Questions</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incidiunt labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida.</p>
+                        <div className="bar" />
+                    </div>
+                    <div className="row align-items-center">
+                        <div className="col-lg-6">
+                            <div className="faq-accordion">
+                                <ul className="accordion">
+                                    <li className="accordion-item">
+                                        <a className="accordion-title active" href="javascript:void(0)">
+                                            <i className="bx bx-chevron-down" />
+                                            What access do I have on the free plan?
+                                        </a>
+                                        <div className="accordion-content show">
+                                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+                                        </div>
+                                    </li>
+                                    <li className="accordion-item">
+                                        <a className="accordion-title" href="javascript:void(0)">
+                                            <i className="bx bx-chevron-down" />
+                                            What access do I have on a free trial?
+                                        </a>
+                                        <div className="accordion-content">
+                                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+                                        </div>
+                                    </li>
+                                    <li className="accordion-item">
+                                        <a className="accordion-title" href="javascript:void(0)">
+                                            <i className="bx bx-chevron-down" />
+                                            Does the price go up as my team gets larger?
+                                        </a>
+                                        <div className="accordion-content">
+                                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+                                        </div>
+                                    </li>
+                                    <li className="accordion-item">
+                                        <a className="accordion-title" href="javascript:void(0)">
+                                            <i className="bx bx-chevron-down" />
+                                            How can I cancel my subscription?
+                                        </a>
+                                        <div className="accordion-content">
+                                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+                                        </div>
+                                    </li>
+                                    <li className="accordion-item">
+                                        <a className="accordion-title" href="javascript:void(0)">
+                                            <i className="bx bx-chevron-down" />
+                                            Can I pay via an Invoice?
+                                        </a>
+                                        <div className="accordion-content">
+                                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="col-lg-6">
+                            <div className="faq-image">
+                                <img src="assets/img/faq.png" alt="image" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </>
     )
 }
--- components/Frontend/TermCondition.tsx
+++ components/Frontend/TermCondition.tsx
@@ -22,23 +22,23 @@ export default function TermCondition() {
                 <div className="container">
                     <div className="single-privacy">
                         <h3 className="mt-0">Introduction</h3>
-                        <p>Welcome to [website name]. By accessing and using this website, you agree to be bound by the terms and conditions set forth below.</p>
+                        <p>Welcome to Start Up. By accessing and using this website, you agree to be bound by the terms and conditions set forth below.</p>
                         <h3 className="mt-0">Acceptable Use</h3>
                         <p>You agree to use this website only for lawful purposes and in accordance with these terms and conditions. You must not use this website in any way that causes, or may cause, damage to the website or impairment of the availability or accessibility of the website.</p>
                         <h3 className="mt-0">Intellectual Property</h3>
-                        <p>The content on this website, including but not limited to text, graphics, images, logos, and software, is the property of [website name] or its affiliates and is protected by United States and international copyright, trademark, and other intellectual property laws.</p>
+                        <p>The content on this website, including but not limited to text, graphics, images, logos, and software, is the property of Start Up or its affiliates and is protected by United States and international copyright, trademark, and other intellectual property laws.</p>
                         <h3 className="mt-0">Disclaimer of Warranties</h3>
-                        <p>This website and its content are provided on an "as is" and "as available" basis, without any warranties of any kind, either express or implied. [Website name] does not warrant that the website will be uninterrupted or error-free, nor does it make any warranty as to the accuracy or reliability of the information contained on the website.</p>
+                        <p>This website and its content are provided on an "as is" and "as available" basis, without any warranties of any kind, either express or implied. Start Up does not warrant that the website will be uninterrupted or error-free, nor does it make any warranty as to the accuracy or reliability of the information contained on the website.</p>
                         <h3 className="mt-0">Limitation of Liability</h3>
-                        <p>In no event shall [website name] or its affiliates be liable for any direct, indirect, incidental, special, or consequential damages arising out of or in connection with the use or inability to use this website.</p>
+                        <p>In no event shall Start Up or its affiliates be liable for any direct, indirect, incidental, special, or consequential damages arising out of or in connection with the use or inability to use this website.</p>
                         <h3 className="mt-0">Indemnification</h3>
-                        <p>You agree to indemnify and hold [website name] and its affiliates harmless from any claims, damages, liabilities, and expenses arising out of your use of this website or your violation of these terms and conditions.</p>
+                        <p>You agree to indemnify and hold Start Up and its affiliates harmless from any claims, damages, liabilities, and expenses arising out of your use of this website or your violation of these terms and conditions.</p>
                         <h3 className="mt-0">Governing Law and Jurisdiction</h3>
                         <p>These terms and conditions shall be governed by and construed in accordance with the laws of the United States and the State of [state name], without giving effect to any principles of conflicts of law. Any legal action or proceeding relating to these terms and conditions shall be brought exclusively in the state or federal courts located in [city, state].</p>
                         <h3 className="mt-0">Changes to Terms and Conditions</h3>
-                        <p>[Website name] reserves the right to modify these terms and conditions at any time and without prior notice. Your continued use of the website following any such modifications constitutes your acceptance of the new terms and conditions.</p>
+                        <p>Start Up reserves the right to modify these terms and conditions at any time and without prior notice. Your continued use of the website following any such modifications constitutes your acceptance of the new terms and conditions.</p>
                         <h3 className="mt-0">Termination</h3>
-                        <p>[Website name] may terminate your access to the website at any time, without notice, for any reason whatsoever.
+                        <p>Start Up may terminate your access to the website at any time, without notice, for any reason whatsoever.
                         </p>
                         <h3 className="mt-0">Contact Information</h3>
                         <p>If you have any questions or concerns regarding these terms and conditions, please contact us at [email address or phone number].</p>
