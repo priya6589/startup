9a02baa2406f5e4dba98299d17fd8ff2c457d490
binarydata123 <binarydata.sale@gmail.com>
2023-04-05 14:26:28 +0530

    latest commit(layouts update)

--- components/Frontend/layouts/Layout.tsx
+++ components/Frontend/layouts/Layout.tsx
@@ -1,5 +1,5 @@
-import Header from "./header";
-import Footer from "./footer";
+import Header from "./Header";
+import Footer from "./Footer";
 import CopyRight from "./Copyright";
 const Layout = ({ children }:any) => {
   return (
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-import FrontendLayout from '../components/Frontend/layouts/layout'
+import FrontendLayout from '../components/Frontend/layouts/Layout'
 export default function App({ Component, pageProps }: AppProps) {
   return(
     <>
